c3675632889f0307838023c2546e1c09d7c463a3
jun <qurugi0347@gmail.com>
2022-03-22 23:19:27 +0900

    Chore: mysql Init scheme 수정

--- docker/test-mysql/init/init.sh
+++ docker/test-mysql/init/init.sh
@@ -4,4 +4,4 @@ MYSQL_SOCKET="/var/run/mysqld/mysqld.sock"
 
 echo "# creating replication user in Master MYSQL"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -S $MYSQL_SOCKET -e "ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -S $MYSQL_SOCKET -e 'CREATE SCHEMA `thegiftingpoint`;'
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -S $MYSQL_SOCKET -e 'CREATE SCHEMA `test`;'
